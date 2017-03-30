dc638a2d1fdbda6e97bf90f2a7828088ea782285
Joseph Cotton <jtcotton63@gmail.com>
2017-03-29 23:58:57 -0600

    Got first state working

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,55 +1,73 @@
-$(document).ready(function(){
-  $("#postComment").click(function(){
-      var myobj = {Name:$("#name").val(),Comment:$("#comment").val()};
-      jobj = JSON.stringify(myobj);
-      $("#json").text(jobj);
-	
-	var url = "comment";
-	$.ajax({
-	url:url,
-	type: "POST",
-	data: jobj,
-	contentType: "application/json; charset=utf-8",
-	success: function(data,textStatus) {
-    	$("#done").html(textStatus);
-		}
-	})
-  });
-    $("#getComments").click(function() {
-    $.getJSON('comment', function(data) {
-      console.log(data);
-      var everything = "<ul>";
-      for(var comment in data) {
-        com = data[comment];
-        everything += "<li> Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
-      }
-      everything += "</ul>";
-      $("#comments").html(everything);
+/**
+ * The Angular controller
+ */
+angular.module('LearnMongoApp',['ui.router'])
+.controller('LearnMongoCtrl', ['$scope', '$http', function($scope, $http) {
+        
+        // First initialize the states array
+        $scope.states = [];
+        
+        // Add all the states to it
+        var state = {
+            codeSnippetStart: "db.createCollection(\"contacts\", ",
+            codeSnippet: "Code snippet",
+            codeSnippetEnd: "});",
+            actionButtonMessage: "Run",
+            nextStateAction: function() {alert('Going to the next state action')}
+        };
+        $scope.states.push(state);
+        
+        
+        // The properties that are displayed in the front end
+        // These first few properties are for displaying the current
+        // state's data. They are the same as the data members found
+        // in the state objects.
+        $scope.displayCodeSnippetStart = "";
+        $scope.displayCodeSnippet = "";
+        $scope.displayCodeSnippetEnd = "";
+        $scope.displayActionButtonMessage = "";
+        $scope.currentNextStateAction = function() {};
+        
+        // These properties 
+        $scope.activeStateIndex = 0;
+        $scope.displayResponseFromServer = "";
+        
+        /**
+         * Applies the state that is in the 
+         */
+        $scope.applyState = function(indexOfStateToApply) {
+            
+            // Set the correct active state index
+            $scope.activeStateIndex = indexOfStateToApply;
+            
+            // Set all the data members from that state
+            // and set their valies in the UI
+            var stateToBeApplied = $scope.states[indexOfStateToApply];
+            $scope.displayCodeSnippetStart = stateToBeApplied.codeSnippetStart;
+            $scope.displayCodeSnippet = stateToBeApplied.codeSnippet;
+            $scope.displayCodeSnippetEnd = stateToBeApplied.codeSnippetEnd;
+            $scope.displayActionButtonMessage = stateToBeApplied.actionButtonMessage;
+            $scope.currentNextStateAction = stateToBeApplied.nextStateAction;
+            
+            // Clear out the server response box
+            $scope.displayResponseFromServer = "";
+            
+            // Apply all the changes we just made
+            $scope.$apply();
+        };
 
-    })
-  })
-
-  $("#deleteComments").click(function(){
-      $("#comments").html("I'm erasing myself");
-	var url = "comment";
-	$.ajax({
-	url:url,
-	type: "DELETE",
-	contentType: "application/json; charset=utf-8",
-	success: function(data,textStatus) {
-    	$("#done").html(textStatus);
-		}
-	})
-	    $.getJSON('comment', function(data) {
-      console.log(data);
-      var everything = "<ul>";
-      for(var comment in data) {
-        com = data[comment];
-        everything += "<li> Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
-      }
-      everything += "</ul>";
-      $("#comments").html(everything);
-
-    })
-  });
-});
+        $scope.getNextActiveStateIndex = function() {
+            
+        }
+        
+        $scope.applyNextState = function() {
+            var nextStateIndex = $scope.getNextActiveStateIndex();
+            $scope.applyState(nextStateIndex);
+        };
+        
+        angular.element(document).ready(function () {
+            console.log("We are ready to apply some state");
+            $scope.applyState(0);
+        });
+    }
+]);
