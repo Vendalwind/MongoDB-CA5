fe9afba8e4d642d3f6f3b6b7e354e2a5cd1e324c
Joseph Cotton <jtcotton63@gmail.com>
2017-03-30 02:16:10 -0600

    Added messages on the side

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,6 +11,7 @@ angular.module('LearnMongoApp',['ui.router'])
         // Add all the states to it
         var state = {
             title: "Create a validated collection",
+            explanation: "<p>In order for a collection to be validated, a <em>validator</em> argument must be passed in when the collection is created.</p><br><br><p>In this case, the validator will check that every document that is inserted into the collection will have the following fields:<ul><li>name</li><li>year_of_birth, with a value less than or equal to 1994</li><li>phone, and that the valiue is an integer</li><li>email, and that the value is a string</li></ul><br><br><p>Run the code at the right to create your first validated collection!</p>",
             codeSnippetStart: "db.createCollection(\"contacts\", ",
             codeSnippet: "{\r\n\t\"validator\": {\r\n\t\t\"$and\": [{\r\n\t\t\t\"name\": {\r\n\t\t\t\t\"$exists\": true\r\n\t\t\t}\r\n\t\t}, {\r\n\t\t\t\"year_of_birth\": {\r\n\t\t\t\t\"$type\": \"int\",\r\n\t\t\t\t\"$lte\": 1994\r\n\t\t\t}\r\n\t\t}, {\r\n\t\t\t\"phone\": {\r\n\t\t\t\t\"$type\": \"int\"\r\n\t\t\t}\r\n\t\t}, {\r\n\t\t\t\"email\": {\r\n\t\t\t\t\"$type\": \"string\"\r\n\t\t\t}\r\n\t\t}]\r\n\t}\r\n}",
             codeSnippetEnd: "});",
@@ -26,6 +27,7 @@ angular.module('LearnMongoApp',['ui.router'])
         
         state = {
             title: "Create a validated collection",
+            explanation: "<p>In order for a collection to be validated, a <em>validator</em> argument must be passed in when the collection is created.</p><br><br><p>In this case, the validator will check that every document that is inserted into the collection will have the following fields:<ul><li>name</li><li>year_of_birth, with a value less than or equal to 1994</li><li>phone, and that the valiue is an integer</li><li>email, and that the value is a string</li></ul><br><br><p>Run the code at the right to create your first validated collection!</p>",
             blankOutResponseFromServer: false,
             actionButtonMessage: "Next",
             nextStateAction: function() {
@@ -36,6 +38,7 @@ angular.module('LearnMongoApp',['ui.router'])
         
         state = {
             title: "Validation in action",
+            explanation: "<p>To see validation in action, try inserting the following document into the collection.<br><br><p><strong>What do you expect will happen? Why?</strong></p><br><br><p>When you think you know the answer, run the code at the right to see what happens.</p>",
             codeSnippetStart: "db.getCollection(\"contacts\").insert({",
             codeSnippet: "{\r\n\t\"name\": \"Bob Smith\",\r\n\t\"year_of_birth\": 1995,\r\n\t\"phone\": 1234567890,\r\n\t\"email\": \"someone@company.com\"\r\n}",
             codeSnippetEnd: "});",
@@ -51,6 +54,7 @@ angular.module('LearnMongoApp',['ui.router'])
         
         state = {
             title: "Validation in action",
+            explanation: "<p>To see validation in action, try inserting the following document into the collection.<br><br><p><strong>What do you expect will happen? Why?</strong></p><br><br><p>When you think you know the answer, run the code at the right to see what happens.</p><br><br><p><strong>The document didn't insert because the validation constraints were not met.</strong></p><br><br><p>Hit next to continue.</p>",
             blankOutResponseFromServer: false,
             actionButtonMessage: "Next",
             nextStateAction: function() {
@@ -61,6 +65,7 @@ angular.module('LearnMongoApp',['ui.router'])
         
         state = {
             title: "Fix the document to pass validation",
+            explanation: "<p>Fix the document at the right so that it passes validation.</p><br><br><p><strong>What modifications do you need to make? Why?</strong></p><br><br><p>Run the code at the right when you think it's ready.</p>",
             blankOutResponseFromServer: true,
             actionButtonMessage: "Run",
             nextStateAction: function() {
@@ -73,6 +78,7 @@ angular.module('LearnMongoApp',['ui.router'])
         
         state = {
             title: "Fix the document to pass validation",
+            explanation: "<p>Congrats, it worked this time!</p>",
             blankOutResponseFromServer: false,
             actionButtonMessage: "Start Over",
             nextStateAction: function() {
@@ -111,6 +117,9 @@ angular.module('LearnMongoApp',['ui.router'])
             
             // Set the title
             $scope.displayTitle = stateToBeApplied.title;
+            
+            // Set the explanation pane on the left-hand side of the page
+            document.getElementById('leftColumn').innerHTML = stateToBeApplied.explanation;
             
             // We will only change the code snippet if we need to
             if(stateToBeApplied.codeSnippet) {
